c9c1f419a9800217571ef34f28b30eb10bc283d9
Omid Najibzadeh <omidn975@gmail.com>
2023-10-17 19:38:51 -0500

    October17 - Omid

--- server.py
+++ server.py
@@ -1,9 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, json
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+
+
+
 
 @app.route('/', methods=['POST'])
-def result():
-    print(request.data)  # raw data
-    print(request.json)  # json (if content-type of application/json is sent with the request)
-    print(request.get_json(force=True))  # json (if content-type of application/json is not sent)
+def default_handler():
+    print("Default")
+    response = Flask.response_class(
+        response="Y",
+        status="201"
+    )
+    return response
+
+
+@app.route('/server', methods=['POST'])
+def server_request_handler():
+    print(request.get_data().decode())
+    print(request.get_json())
+
+    data = "SecretCode"
+    response = Flask.response_class(
+        response=data,
+        status="202"
+    )
+    return response
